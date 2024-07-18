659f653891f609944db4792fbc5f6563bddb53e8
horacenjoroge <125595726+horacenjoroge@users.noreply.github.com>
2024-07-19 01:50:28 +0300

    added changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
               <div className='absolute w-28 left-0 -top-20 hidden lg:block'>
                 {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
                 <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
-                <img src='/snake-1.png' className='w-full' />
+                <img src='users/snake-1.png' className='w-full' />
               </div>
               <h1 className='relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl'>
                 Your Image on a{' '}
@@ -51,27 +51,27 @@ export default function Home() {
                 <div className='flex -space-x-4'>
                   <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-1.png'
+                    src='/testimonials/1.jpg'
                     alt='user image'
                   />
                   <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-2.png'
+                    src='/testimonials/2.jpg'
                     alt='user image'
                   />
                   <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-3.png'
+                    src='/testimonials/3.jpg'
                     alt='user image'
                   />
                   <img
                     className='inline-block h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-4.jpg'
+                    src='/testimonials/4.jpg'
                     alt='user image'
                   />
                   <img
                     className='inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100'
-                    src='/users/user-5.jpg'
+                    src='/testimonials/5.jpg'
                     alt='user image'
                   />
                 </div>
@@ -96,11 +96,11 @@ export default function Home() {
           <div className='col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit'>
             <div className='relative md:max-w-xl'>
               <img
-                src='/your-image.png'
+                src='/users/your-image.png'
                 className='absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block'
               />
               <img
-                src='/line.png'
+                src='/users/line.png'
                 className='absolute w-20 -left-6 -bottom-6 select-none'
               />
               <Phone className='w-64' imgSrc='/testimonials/1.jpg' />
@@ -121,7 +121,7 @@ export default function Home() {
               </span>{' '}
               say
             </h2>
-            <img src='/snake-2.png' className='w-24 order-0 lg:order-2' />
+            <img src='/users/snake-2.png' className='w-24 order-0 lg:order-2' />
           </div>
 
           <div className='mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16'>
@@ -147,7 +147,7 @@ export default function Home() {
               <div className='flex gap-4 mt-2'>
                 <img
                   className='rounded-full h-12 w-12 object-cover'
-                  src='/users/user-1.png'
+                  src='/testimonials/2.jpg'
                   alt='user'
                 />
                 <div className='flex flex-col'>
@@ -184,7 +184,7 @@ export default function Home() {
               <div className='flex gap-4 mt-2'>
                 <img
                   className='rounded-full h-12 w-12 object-cover'
-                  src='/users/user-4.jpg'
+                  src='/testimonials/4.jpg'
                   alt='user'
                 />
                 <div className='flex flex-col'>
@@ -221,18 +221,18 @@ export default function Home() {
           <div className='mx-auto max-w-6xl px-6 lg:px-8'>
             <div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
               <img
-                src='/arrow.png'
+                src='/users/arrow.png'
                 className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
               />
 
               <div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
                 <img
-                  src='/horse.jpg'
+                  src='/users/horse.jpg'
                   className='rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full'
                 />
               </div>
 
-              <Phone className='w-60' imgSrc='/horse_phone.jpg' />
+              <Phone className='w-60' imgSrc='/users/horse_phone.jpg' />
             </div>
           </div>
 
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -156,7 +156,7 @@ export function Reviews() {
     <MaxWidthWrapper className='relative max-w-5xl'>
       <img
         aria-hidden='true'
-        src='/what-people-are-buying.png'
+        src='/users/what-people-are-buying.png'
         className='absolute select-none hidden xl:block -left-32 top-1/3'
       />
 
